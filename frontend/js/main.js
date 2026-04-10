dba1d9d2ffe9dbdc56a1c21877b338d20887bca1
Diego30k-tec <diegoarmandoruiz99@gmail.com>
2026-04-10 15:18:13 -0500

    :(

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,9 +3,29 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch('http://localhost:8080/api/clientes')
         .then(response => response.json())
         .then(data => {
-            console.log('Datos:', data);
-            const elemento = document.getElementById("tabla-cliente")
-            elemento.innerHTML = JSON.stringify(data);
-            console.log(elemento)
+
+            //console.log('Datos:', data);
+            const elemento = document.getElementById("table-cliente")
+            //elemento.innerHTML = JSON.stringify(data);
+            //console.log(elemento)
+
+            //DOM -> <tbody id="table-cliente">
+            for (let i = 0; i < data.length; i++) {
+                //console.log(data[i]) Muestra en la consola
+                //(data[i]) muestra en forma de array
+                let cliente = data[i]
+                // alt + 96
+                let fila = `
+                            <tr>
+                            <td>${cliente.id}</td>
+                            <td>${cliente.apellido}</td>
+                            <td>${cliente.nombre}</td>
+                            <td>${cliente.dni}</td>
+                            <td>${cliente.telefono}</td>
+                            <td>${cliente.direccion}</td>
+                            </tr>
+                            `
+                console.log(data[i])
+            }
         })
 });
